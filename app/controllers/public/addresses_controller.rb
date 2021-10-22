455da28af29cf1e6f150b99475fa3eb8fca687a5
RyokoFJBYS <ryokofujibayashi828@gmail.com>
2021-10-22 16:06:51 +0000

    [Add]配送先登録・一覧ページ

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,22 @@
 class Public::AddressesController < ApplicationController
   def index
+    @addresses = Address.all
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to addresses_path
   end
 
   def edit
   end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
+  end
+
 end
